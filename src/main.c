8c6084d501572c3a74887aaf71971794e30e92b1
Fredrik Hansson <fredrikhansson_12345@yahoo.com>
2021-03-16 23:06:29 +0100

    supersample the backbuffer

--- src/main.c
+++ src/main.c
@@ -164,6 +164,7 @@ int main(int argc, char* argv[])
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES,4);
 
     if(options.outputfile != 0)
     {
